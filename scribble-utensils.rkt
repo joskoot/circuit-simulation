0a9630657f67af09af6b40c7b4de2c50651b59b6
Jos Koot <jos.koot@gmail.com>
2024-05-04 22:30:58 +0200

    Add files via upload

--- scribble-utensils.rkt
+++ scribble-utensils.rkt
@@ -1,90 +1,69 @@
 #lang scribble/manual
 
+@; By Jacob J. A. Koot
+
 @;====================================================================================================
 
 @(require
-   scribble/base
-   scribble/core
-   scribble/eval
-   racket
-   "simulator.rkt"
-   (for-syntax racket)
-   (for-label racket "simulator.rkt"))
+  scribble/core
+  scribble/eval
+  scribble/racket
+  racket
+  racket/block
+  "circuits.rkt"
+  (for-label "circuits.rkt" (except-in racket set) racket/block racket/function)
+  (for-template "circuits.rkt" racket/function (except-in racket set))
+  (for-syntax racket/function (except-in racket set) racket/block))
 
-@(provide
-   (all-defined-out)
-   (all-from-out scribble/base)
-   (all-from-out scribble/core scribble/eval))
+@(provide (all-defined-out))
 
-@(print-reader-abbreviations #f)
-@(print-as-expression #f)
+@(define-syntax-rule (Interaction x ...)
+  (interaction #:eval
+   (make-base-eval
+    #:lang '(begin
+             (require racket "circuits.rkt" racket/block
+                      (for-syntax racket racket/block))
+             (print-as-expression #f))) x ...))
 
-@(define-for-syntax local #t)
-
-@(define-syntax (nbhll stx)
-   (syntax-case stx ()
-     ((_ x y ...)
-      (if local
-        #'(nb (hyperlink x y ...))
-        #'(nb (hyperlink (string-append "../../" x) y ...))))))
-
-@(define-syntax (Defmodule stx)
-   (if local
-     #'(defmodule "simulator.rkt" #:packages ())
-     #'(defmodule processor/simulator #:packages ())))
-
-@(define-syntax-rule
-   (Interaction x ...)
-   (interaction/no-prompt
-     #:eval
-     (make-base-eval
-       #:pretty-print? #f
-       #:lang
-       '(begin
-          (require racket/base "simulator.rkt")
-          (print-reader-abbreviations #f)
-          (define (hex str) (read (open-input-string str)))))
-     x ...))
-
-@(define-syntax-rule
-   (Interaction* x ...)
-   (interaction/no-prompt #:eval evaller x ...))
+@(define-syntax-rule (Interaction* x ...)
+  (interaction #:eval evaller x ...))
 
 @(define (make-evaller)
-   (make-base-eval
-     #:pretty-print? #f
-     #:lang
-     '(begin
-        (require racket/base "simulator.rkt")
-        (print-reader-abbreviations #f)
-        (define (hex str) (read (open-input-string str))))))
+  (make-base-eval
+   #:lang '(begin
+            (require racket "circuits.rkt" racket/block
+                     (for-syntax racket racket/block))
+            (print-as-expression #f))))
 
-@(define-syntax-rule (cmt x ...) (black (smaller x ...)))
-@(define (itt . content) (italic (apply tt content)))
-@(define (btt . content) (bold (apply tt content)))
 @(define evaller (make-evaller))
 @(define (reset-Interaction*) (set! evaller (make-evaller)))
+@(define-syntax-rule (Elemtag x) (add-elem-tag x))
+
+@(define add-elem-tag
+  (let ((tags '()))
+   (λ ((x 'not-present))
+    (cond
+     ((eq? x 'not-present) tags)
+     (else (set! tags (cons x tags)) (elemtag x))))))
+
 @(define lb linebreak)
 @(define nb nonbreaking)
 @; ignore is a syntax such as to prevent arguments to be evaluated.
-@(define-syntax-rule (ignore x ...) (element #f '()))
+@(define-syntax-rule (ignore x ...) (void))
+@(define-syntax-rule (do-not-ignore x ...) (begin x ...))
 @; Below syntaxes are used such as to allow keyword arguments
 @; without explicitly mentioning them in the definitions.
 @(define-syntax-rule (nbsl x ...) (nb (seclink    x ...)))
 @(define-syntax-rule (nbsr x ...) (nb (secref     x ...)))
 @(define-syntax-rule (nbhl x ...) (nb (hyperlink  x ...)))
 @(define-syntax-rule (nber x ...) (nb (elemref    x ...)))
-@(define-syntax-rule (Nber x y ...) (nb (elemref x (tt (list y ...)))))
 @(define-syntax-rule (nbrl x ...) (nb (racketlink x ...)))
 @(define-syntax-rule (nbr  x ...) (nb (racket     x ...)))
 @(define-syntax-rule (nbpr x) (nber x (tt x)))
-@(define-syntax-rule (Nbpr x y) (nber x (tt y)))
-@(define-syntax-rule (defmacro x ...) (defform #:kind "macro" #:link-target? #f x ...))
-@(define-syntax-rule (defmacro* x ...) (defform* #:kind "macro" #:link-target? #f x ...))
-@(define-syntax-rule (deffun x ...) (defproc #:kind "procedure" #:link-target? #f x ...))
-@(define-syntax-rule (defpred x ...) (defproc #:kind "predicate" #:link-target? #f x ...))
+@(define-syntax-rule (defmacro x ...) (defform #:kind "macro" x ...))
+@(define-syntax-rule (defmacro* x ...) (defform* #:kind "macro" x ...))
 @(define (tt . content) (element 'tt (apply list content)))
-@(define(minus) (tt "−"))
+@(define(minus) (tt "-"))
 @(define(-?) (element "roman" ?-))
 @(define (note . x) (inset (apply smaller x)))
 @(define (inset . x) (apply nested #:style 'inset x))
@@ -94,27 +73,26 @@
 @(define-syntax-rule (Tabular ((e ...) ...) . rest) (tabular (list (list e ...) ...) . rest))
 @(define (roman . x) (element 'roman x))
 @(define (nbtt x) (nb (ttblack x)))
+@(define test-arg   (make-parameter "yet to be constructed"))
+@(define test-thunk (make-parameter "yet to be constructed"))
+@(define test-block (make-parameter "yet to be constructed"))
 
-@(define Void
-   (let
-     ((x
-        (seclink
-          "void"
-          #:doc '(lib "scribblings/reference/reference.scrbl") (nb (tt "#<void>")))))
-     (λ () x)))
+@(define Void (let ((x
+@seclink["void" #:doc '(lib "scribblings/reference/reference.scrbl") (nb (tt "#<void>"))]))
+              (λ () x)))
 
 @(define-syntax-rule (Tabular-with-linebreaks ((e ...) ... (le ...)) . rest)
-   (Tabular (((list e (lb) (hspace 1)) ...) ... (le ...)) . rest))
+  (Tabular (((list e (lb) (hspace 1)) ...) ... (le ...)) . rest))
 
 @(define (make-color-style color)
-   (define prop:color (color-property color))
-   (define color-style (style #f (list prop:color)))
-   (lambda elems (element 'roman (element color-style elems))))
+  (define prop:color (color-property color))
+  (define color-style (style #f (list prop:color)))
+  (lambda elems (element 'roman (element color-style elems))))
 
 @(define (make-ttcolor-style color)
-   (define prop:color (color-property color))
-   (define color-style (style #f (list prop:color)))
-   (lambda elems (element 'tt (element color-style elems))))
+  (define prop:color (color-property color))
+  (define color-style (style #f (list prop:color)))
+  (lambda elems (element 'tt (element color-style elems))))
 
 @(define red       (make-color-style   "red"))
 @(define green     (make-color-style   "green"))
@@ -123,22 +101,106 @@
 @(define ttblack   (make-ttcolor-style "black"))
 @(define ttred     (make-ttcolor-style "red"))
 @(define ttgreen   (make-ttcolor-style "green"))
-@(define rblue     (make-color-style '(0 0 180)))
 @(define optional "optional, evaluated, default: ")
 @(define opt-proc "optional, default: ")
-@(define-syntax-rule (sgreen x ...) (green (smaller x) ...))
-@(define-syntax-rule (sred x ...) (red (smaller x) ...))
 
-@(define (Rckt) (nbhl "https://docs.racket-lang.org/reference/index.html" "Racket"))
-@(define (DrRckt) (nbhl "https://docs.racket-lang.org/drracket/index.html" "DrRacket"))
+@(define (Rckt) (nbhl "https://racket-lang.org/" "Racket"))
+
 @(define (keyword . x)
-   (apply seclink "keywords" #:doc '(lib "scribblings/reference/reference.scrbl") x))
+  (apply seclink "keywords" #:doc '(lib "scribblings/reference/reference.scrbl") x))
 
-@(define-syntax-rule (Elemtag x) (add-elem-tag x))
-@(define elem-fetch-tag (gensym))
-@(define add-elem-tag (let ((tags '())) (λ ((x elem-fetch-tag))
-                                          (cond
-                                            ((eq? x elem-fetch-tag) tags)
-                                            (else (set! tags (cons x tags))
-                                              (elemtag x))))))
+@(define-syntax SET
+  (make-element-id-transformer
+   (lambda _ #'(tt "set"))))
+
+@(define-syntax RESET
+  (make-element-id-transformer
+   (lambda _ #'(tt "reset"))))
+
+@(define-syntax FIRST
+  (make-element-id-transformer
+   (lambda _ #'(tt "first"))))
+
+@(define-syntax SECOND
+  (make-element-id-transformer
+   (lambda _ #'(tt "second"))))
+
+@(define-syntax ELSE
+  (make-element-id-transformer
+   (lambda _ #'(tt "else"))))
+
+@(define-syntax VALUES
+  (make-element-id-transformer
+   (lambda _ #'(tt "values"))))
+
+@(define-syntax TIME
+  (make-element-id-transformer
+   (lambda _ #'(tt "time"))))
+
+@(define-syntax DELAY
+  (make-element-id-transformer
+   (lambda _ #'(tt "delay"))))
+
+@(define-syntax BITS
+  (make-element-id-transformer
+   (lambda _ #'(tt "bits"))))
+
+@; With thanks to Dupéron Georges
+@(define (defform-remove-empty-lines the-defform)
+  (define 5blanks
+   (make-list 5 (paragraph (style #f '(omitable)) (element #f (list (element 'tt '(nbsp)))))))
+  (match the-defform
+   [(box-splice
+     (list
+      before ...
+      (nested-flow nf-style
+       (list
+        (table t-style
+         (list other ...
+          (list
+           (table (style "specgrammar" tspec-style)
+           (list lines ...)))
+          more ...))))
+      after ...))
+    (define without-empty-lines
+     ;; an empty lines is a sequence of five empty columns:
+     (remove* (list 5blanks) lines))
+    (box-splice
+     (append
+      before
+      (list
+       (nested-flow nf-style
+        (list
+         (table t-style
+          (append other
+           (list
+            (list (table (style "specgrammar" tspec-style) without-empty-lines))) more)))))
+      after))]))
+
+@(define-syntax (make-truth-tables stx)
+  (syntax-case stx ()
+   ((_ (in ...) (gate proc) ...)
+  #'(let*
+     ((inputs (sort (for*/list ((in in-trits) ...) (list in ...)) X-inputs<?))
+      (header (list (t 'Inputs) (nbr gate) ...))
+      (table
+       (for/list ((input (in-list inputs)))
+        (cons (t input)
+         (for/list ((p (in-list (list proc ...)))) (t (apply p input))))))
+      (row-props
+      `((top-border bottom-border) ,@(make-list (sub1 (length inputs)) '()) 'bottom-border))
+      (column-props (make-list (length header) 'center)))
+     (tabular (cons header table)
+      #:sep (hspace 3)
+      #:row-properties row-props
+      #:column-properties column-props)))))
+
+@(define (t x) 
+  (tt
+   (if (list? x)
+    (apply format (case (length x) ((1) "~a") ((2) "~a ~a") ((3) "~a ~a ~a")) x)
+    (format "~a" x))))
+
+@(define (X-inputs<? x y)
+  (< (count ?? x) (count ?? y)))
 
