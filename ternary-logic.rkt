28215b0d6fab2dfaba741e70cc7ebaa9fef663db
Jos Koot <jos.koot@gmail.com>
2024-09-07 08:13:53 +0200

    Add files via upload

--- ternary-logic.rkt
+++ ternary-logic.rkt
@@ -1,106 +1,77 @@
-#lang racket
+#lang racket/base
 
-;=====================================================================================================
+(require (only-in racket natural? count))
 
 (provide
-  F  T  ?
-  F? T? ??
-  trit? trits in-trits trit-case
-  bit?  bits  in-bits  bit-case
+  F T ? F? T? ??
+  trit? bit?
+  trits bits
+  in-trits in-bits
+  trit-case bit-case
   trit-combinations bit-combinations)
 
-;=====================================================================================================
+(define-syntax-rule (define-bit/trit bit/trit name pred)
+  (define-values (name pred)
+    (let ()
+      (struct name bit/trit ()
+        #:omit-define-syntaxes
+        #:property prop:object-name (λ (ignore) 'name)
+        #:property prop:custom-write (λ (o p m) (write 'name p)))
+      (values (name) pred))))
 
 (struct trit ())
 (struct bit trit ())
-
-(define (make-trit-writer trit-symbol)
-  (define (trit-writer trit port mode) (write trit-symbol port))
-  trit-writer)
-
-(struct F bit ()
-  #:property prop:custom-write (make-trit-writer 'F)
-  #:name trit-F
-  #:constructor-name make-trit-F)
-
-(struct T bit ()
-  #:property prop:custom-write (make-trit-writer 'T)
-  #:name trit-T
-  #:constructor-name make-trit-T)
-
-(struct ? trit ()
-  #:property prop:custom-write (make-trit-writer '?)
-  #:name trit-?
-  #:constructor-name make-trit-?)
-
-(define F (make-trit-F))
-(define T (make-trit-T))
-(define ? (make-trit-?))
+(define-bit/trit bit F F?)
+(define-bit/trit bit T T?)
+(define-bit/trit trit ? ??)
 (define trits (list F T ?))
-(define bits  (list F T))
+(define bits (list F T))
 (define in-trits (in-list trits))
-(define in-bits  (in-list bits))
+(define in-bits (in-list bits))
 
-(define (trit-select trit F-thunk T-thunk ?-thunk)
-  (cond
-    ((F? trit) (F-thunk))
-    ((T? trit) (T-thunk))
-    ((?? trit) (?-thunk))
-    (else (raise-argument-error 'trit-select "trit?" trit))))
-
-(define (bit-select bit F-thunk T-thunk)
-  (cond
-    ((F? bit) (F-thunk))
-    ((T? bit) (T-thunk))
-    (else (raise-argument-error 'bit-select "bit?" bit))))
-
-(define-syntax (trit-case stx)
-  (syntax-case stx ()
-    ((_ trit (F-case ...) (T-case ...) (?-case ...))
-     #'(trit-select trit (thunk F-case ...) (thunk T-case ...) (thunk ?-case ...)))))
-
-(define-syntax-rule (bit-case bit (F-case ...) (T-case ...))
-  (bit-select bit (thunk F-case ...) (thunk T-case ...)))
-
-(define-syntax (thunk stx)
-  (syntax-case stx ()
-    ((_) #'void)
-    ((_ expr ...) #'(λ () expr ...))))
-
-(define (trit-combinations n (sort? #f) #:vector (vector? #f))
-  (unless (natural? n) (raise-argument-error 'trit-combinations "natural?" n))
-  (define (trit-combinations n)
+(define-syntax-rule (trit-case trit (F-body ...) (T-body ...) (?-body ...))
+  (let ((t trit))
     (cond
-      ((zero? n) '(()))
-      (else
-        (define trit-lists (trit-combinations (sub1 n)))
-        (append
-          (map cons-F trit-lists)
-          (map cons-T trit-lists)
-          (map cons-? trit-lists)))))
-  (define trit-lists (trit-combinations n))
-  (define combinations (if sort? (sort trit-lists trit-list<?) trit-lists))
-  (if vector? (list->vector combinations) combinations))
+      ((F? t) F-body ...)
+      ((T? t) T-body ...)
+      ((?? t) ?-body ...)
+      (else (raise-argument-error 'trit-case "trit?" t)))))
+
+(define-syntax-rule (bit-case trit (F-body ...) (T-body ...))
+  (let ((t trit))
+    (cond
+      ((F? t) F-body ...)
+      ((T? t) T-body ...)
+      (else (raise-argument-error 'bit-case "bit?" t)))))
+
+(define (trit-combinations n #:sort (sort? #f) #:vector (vector? #f))
+  (unless (natural? n) (raise-argument-error 'trit-combinations "natural?" n))
+  (define combis (combinations trits n))
+  (case (list (and sort? #t) (and vector? #t))
+    (((#f #f)) combis)
+    (((#f #t)) (apply vector combis))
+    (((#t #f)) (sort combis combination<?))
+    (((#t #t)) (apply vector (sort combis combination<?)))))
 
 (define (bit-combinations n #:vector (vector? #f))
-  (unless (natural? n) (raise-argument-error 'list-lists-of-trits "natural?" n))
-  (define (bit-combinations n)
-    (cond
-      ((zero? n) '(()))
-      (else
-        (define bit-lists (bit-combinations (sub1 n)))
-        (append
-          (map cons-F bit-lists)
-          (map cons-T bit-lists)))))
-  (define combinations (bit-combinations n))
-  (if vector? (list->vector combinations) combinations))
+  (unless (natural? n) (raise-argument-error 'bit-combinations "natural?" n))
+  (define combis (combinations bits n))
+  (cond
+    (vector? (apply vector combis))
+    (else combis)))
 
-(define (cons-F list-of-trits) (cons F list-of-trits))
-(define (cons-T list-of-trits) (cons T list-of-trits))
-(define (cons-? list-of-trits) (cons ? list-of-trits))
+(define (combinations lst n)
+  (cond
+    ((zero? n) '(()))
+    ((null? lst) '())
+    (else
+      (define in-lst (in-list lst))
+      (define (combinations n)
+        (cond
+          ((zero? n) (map list lst))
+          (else
+            (define in-combis (in-list (combinations (sub1 n))))
+            (for*/list ((e in-lst) (combi in-combis)) (cons e combi)))))
+      (combinations (sub1 n)))))
 
-(define (trit-list<? trit-list-x trit-list-y)
-  (< (count ?? trit-list-x) (count ?? trit-list-y)))
-        
-;=====================================================================================================
-; The end
+(define (combination<? a b) (< (count ?? a) (count ?? b)))
